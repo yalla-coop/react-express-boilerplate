b71abc92e8b038846a8790b253ea1717699550be
fadi <fadeomar2015@gmail.com>
2021-03-23 17:32:22 +0200

    setup storybook

---
+++ client/.storybook/theme-decorator.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { ThemeProvider } from '@emotion/react';
+import theme from "./../src/theme"
+
+import 'antd/dist/antd.css';
+
+
+const style = {
+  width: '100%',
+  minHeight: "100vh",
+}
+
+const ThemeDecorator = storyFn => (
+  <ThemeProvider theme={theme}><div style={style}>{storyFn()}</div></ThemeProvider>
+)
+
+export default ThemeDecorator
